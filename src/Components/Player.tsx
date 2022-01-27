d47366f707399096fa4bf44e2efdb7dba5322a1b
EtienneBrJ <etiennebrxv@gmail.com>
2022-01-27 04:28:27 +0100

    conditions display buttons

--- src/Components/Player.tsx
+++ src/Components/Player.tsx
@@ -1,11 +1,9 @@
-import { calculateScore } from "../utils";
 import '../Styles/Player.css'
 
 interface IPlayer {
     chips: number;
     cards: any[];
     score: number;
-    bet: number;
     startDeal: any;
     stand: any;
     hit: any;
@@ -13,11 +11,11 @@ interface IPlayer {
     state: number;
 }
 
-const Player: React.FC<IPlayer> = ({ chips, cards, score, bet, startDeal, stand, hit, setBet, state }) => {
+const Player: React.FC<IPlayer> = ({ chips, cards, score, startDeal, stand, hit, setBet, state }) => {
     return (
         <div className="playerContainer">
             <div className="playerInfo">
-                <p className="score">Player's score: {calculateScore(cards)}</p>
+                <p className="score">Player's score: {score}</p>
                 <p className="bankroll">${chips}</p>
             </div>
             <div className="cardsContainer">
@@ -28,15 +26,18 @@ const Player: React.FC<IPlayer> = ({ chips, cards, score, bet, startDeal, stand,
                 ))}
             </div>
             <div className="controlContainer">
-                <div className="bet">
-                    <input type="text" placeholder="  Your bet" onChange={e => setBet(parseInt(e.target.value))} />
-                    <button onClick={() => startDeal()}>Start Deal</button>
-                </div>
-                <div className="action">
-
-                    <button onClick={() => hit()}>Hit</button>
-                    <button onClick={() => stand()}>Stand</button>
-                </div>
+                {state === 0 || state === 5 ?
+                    <div className="bet">
+                        <input type="text" placeholder="  Your bet" onChange={e => setBet(parseInt(e.target.value))} />
+                        <button onClick={() => startDeal()}>Start Deal</button>
+                    </div>
+                    : null}
+                {state === 2 ?
+                    <div className="action">
+                        <button onClick={() => hit()}>Hit</button>
+                        <button onClick={() => stand()}>Stand</button>
+                    </div>
+                    : null}
 
             </div>
         </div>
