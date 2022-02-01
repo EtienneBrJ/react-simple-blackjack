1904d6589d3d8bbb3ab9e9fb9d4af7f898a89cb1
EtienneBrJ <etiennebrxv@gmail.com>
2022-02-01 05:48:06 +0100

    move the states back to the parent component

--- src/Components/Leaderboard.tsx
+++ src/Components/Leaderboard.tsx
@@ -6,44 +6,27 @@ import { doc, setDoc, collection, getDocs, getDoc } from "firebase/firestore";
 import { db } from '../firebase-config'
 
 
+const blackjackCollection = collection(db, "blackjack");
+
 interface Board {
     user: string;
     score: number;
     setUser: any;
+    leaderboard?: { user: string; score: number; }[];
+    setLeaderboard?: any;
 }
 
 const BoardButton: React.FC<Board> = ({ user, setUser, score }) => {
 
 
     const [isClicked, setIsClicked] = useState(false)
+    const [leaderboard, setLeaderboard] = useState<{ user: string; score: number; }[]>([{ user: "False Data", score: 100 }, { user: "2 much req", score: 212 }, { user: "on the db", score: 121 }])
 
     const toggleBoard = () => {
         setIsClicked(!isClicked)
     }
 
-    return <div className='leaderboardContainer'>
-        <button className='leaderboardBtn' onClick={toggleBoard} >
-            {isClicked ? <FontAwesomeIcon className='cross' icon={faTimes} /> : <FontAwesomeIcon className='trophy' icon={faTrophy} />}
-        </button>
-        {isClicked ? <Leaderboard user={user} setUser={setUser} score={score} /> : null}
-    </div>
-
-}
-
-
-const Leaderboard: React.FC<Board> = ({ user, setUser, score }) => {
-
-
-    const [leaderboard, setLeaderboard] = useState<{ user: string; score: number; }[]>([])
-    const blackjackCollection = collection(db, "blackjack");
-
-
     useEffect(() => {
-        const getLeaderboard = async () => {
-            const data = await getDocs(blackjackCollection);
-            setLeaderboard(data.docs.map((doc) => ({ user: doc.data().user, score: doc.data().score })))
-        }
-        getLeaderboard();
 
         const updateScore = async () => {
             const docRef = doc(blackjackCollection, user + '\'s score')
@@ -60,11 +43,29 @@ const Leaderboard: React.FC<Board> = ({ user, setUser, score }) => {
             }
         }
         updateScore();
-    }, [blackjackCollection, leaderboard, user, score])
+
+        if (isClicked) {
+            console.log('Entrée useEffect isClicked est true -')
+            const getLeaderboard = async () => {
+                const data = await getDocs(blackjackCollection);
+                setLeaderboard(data.docs.map((doc) => ({ user: doc.data().user, score: doc.data().score })))
+                console.log(leaderboard)
+            }
+        }
+    })
+
+    return <div className='leaderboardContainer'>
+        <button className='leaderboardBtn' onClick={toggleBoard} >
+            {isClicked ? <FontAwesomeIcon className='cross' icon={faTimes} /> : <FontAwesomeIcon className='trophy' icon={faTrophy} />}
+        </button>
+        {isClicked ? <Leaderboard user={user} setUser={setUser} score={score}
+            setLeaderboard={setLeaderboard} leaderboard={leaderboard} /> : null}
+    </div>
+
+}
 
 
-
-
+const Leaderboard: React.FC<Board> = ({ user, setUser, score, leaderboard }) => {
 
     const addUserDocument = async () => {
         if (user !== 'Player') {
@@ -75,18 +76,28 @@ const Leaderboard: React.FC<Board> = ({ user, setUser, score }) => {
         }
     }
 
-
     return <div className='leaderboardContent'>
         <div className='leaderboardDisplay'>
             <h1>Leaderboard</h1>
             <div className='leaderboard'>
+                <div className='rankColumn'>
+                    <p className='title'>Rank</p>
+                    {leaderboard
+                        ? leaderboard.sort((a, b) => b.score - a.score).map((line, idx) => <p key={line.user}>{idx + 1}</p>)
+                        : null}
+
+                </div>
                 <div className='userColumn'>
-                    <p className='title'>Username</p>
-                    {leaderboard.map((line) => <p key={line.user}>{line.user}</p>)}
+                    <p className='title'>Name</p>
+                    {leaderboard
+                        ? leaderboard.sort((a, b) => b.score - a.score).map((line) => <p key={line.user}>{line.user}</p>)
+                        : null}
                 </div>
                 <div className='scoreColumn'>
-                    <p className='title'>Bankroll</p>
-                    {leaderboard.sort((a, b) => b.score - a.score).map((line) => <p key={line.user}>{line.score}</p>)}
+                    <p className='title'>Score</p>
+                    {leaderboard
+                        ? leaderboard.sort((a, b) => b.score - a.score).map((line) => <p key={line.user}>{line.score}</p>)
+                        : null}
                 </div>
             </div>
             <div className='leaderboardRegister'>
